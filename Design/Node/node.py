86fc770dc0f202ccba0b6877188e891aa3bf89e6
Jaehoon Hwang <jae@Jay.local>
2015-12-27 00:37:57 -0800

    Node Module Created
    Bi-directional Node, includes:
    - Name
    - Value
    - Prev
    - Next

---
+++ Design/Node/node.py
@@ -0,0 +1,81 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class Node(object):
+
+	# Constructor
+	def __init__(self, name = None, value = None, prev = None, next = None):
+		logging.debug('Node::Constructor Initialized')
+
+		self.Name = name
+		logging.debug(('Node::Constructor::Name: ', self.Name))
+		self.Value = value
+		logging.debug(('Node::Constructor::Value: ', self.Value))
+		self.Prev= prev
+		logging.debug(('Node::Constructor::Prev: ', self.Prev))
+		self.Next = next
+		logging.debug(('Node::Constructor::Next: ', self.Next))
+
+		logging.debug('Node::Consturctor Finalized')
+
+	## Return Variables
+
+	def returnName(self):
+		logging.debug(('Node::Return::Name: ', self.Name))
+		return self.Name
+
+	def returnValue(self):
+		logging.debug(('Node::Return::Value: ', self.Value))
+		return self.Value
+
+	def returnPrev(self):
+		logging.debug(('Node::Return::Prev: ', self.Prev))
+		return self.Prev
+
+	def returnNext(self):
+		logging.debug(('Node::Return::Next: ', self.Next))
+		return self.Next
+
+	## Set Variables
+
+	def setName(self, n):
+		logging.debug('Node::Set::Name Initialized')
+		logging.debug(('Node::Set::Name Current: ', self.Name))
+		logging.debug(('Node::Set::Name Target : ', n))
+		self.Name = n
+		logging.debug(('Node::Set::Name Changed: ', self.Name))
+		logging.debug('Node::Set::Name Finished')
+
+	def setValue(self, v):
+		logging.debug('Node::Set::Value Initialized')
+		logging.debug(('Node::Set::Value Current: ', self.Value))
+		logging.debug(('Node::Set::Value Target : ', v))
+		self.Value = v
+		logging.debug(('Node::Set::Value Changed: ', self.Value))
+		logging.debug('Node::Set::Value Finished')
+
+	def setPrev(self, p):
+		logging.debug('Node::Set::Prev Initialized')
+		logging.debug(('Node::Set::Prev Current: ', self.Prev))
+		logging.debug(('Node::Set::Prev Target:  ', p))
+		self.Prev = p 
+		logging.debug(('Node::Set::Prev Changed: ', self.Prev))
+		logging.debug('Node::Set Prev Finished')
+
+
+	def setNext(self, n):
+		logging.debug('Node::Set::Next Initialized')
+		logging.debug(('Node::Set::Next Current: ', self.Next))
+		logging.debug(('Node::Set::Next Target : ', n))
+		self.Next = n 
+		logging.debug(('Node::Set::Next Changed: ', self.Next))
+		logging.debug('Node::Set::Next Finished')
+
+	def setAll(self, name, value, prev, n):
+		logging.debug('Node::Set::All Initialized')
+		self.setName(name)
+		self.setValue(value)
+		self.setPrev(prev)	
+		self.setNext(n)
+		logging.debug('Node::Set::All Finished')
---
+++ Design/Node/node_test.py
@@ -0,0 +1,37 @@
+from node import Node
+
+print ('Intializing 2 classes: test1 and test2')
+
+test1 = Node('foo', 123)
+print ('Return Test1')
+print (test1.returnName())
+print (test1.returnValue())
+print (test1.returnPrev())
+print (test1.returnNext())
+
+test2 = Node('foo2', 456, test1, test1)
+print ('Return Test2')
+print (test2.returnName())
+print (test2.returnValue())
+print (test2.returnPrev())
+print (test2.returnNext())
+
+print('Testing setAll()')
+test1.setAll('foo_changed', 789, test2, test2)
+print (test1.returnName())
+print (test1.returnValue())
+print (test1.returnPrev())
+print (test1.returnNext())
+
+print('Testing Access of Pointer Previous and Next')
+
+test2_name_p = test1.returnPrev().returnName()
+print('With Prev, Name: ', test2_name_p)
+
+test2_name = test1.returnNext().returnName()
+print('With Next, Name: ', test2_name)
+
+test2_foop = test1.returnPrev()
+test2_foo = test1.returnNext()
+print('With test2_foop::Name: ', test2_foop.returnName())
+print('With test2_foo::Name : ',test2_foo.returnName())		
