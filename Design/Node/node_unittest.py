f464e137e45bf9d9fafd02b67d07de465a349cfa
Jaehoon <jaehoon217@gmail.com>
2015-12-31 00:13:04 -0800

    Updated Unit Test File and .gitignore
    Unittest file content includes return, set, and setall functions
    .gitignore contains .pyo now

--- Design/Node/node_unittest.py
+++ Design/Node/node_unittest.py
@@ -1,7 +1,12 @@
+import logging
 import unittest
 from node import Node
 
+root_logger = logging.getLogger()
+root_logger.disabled = True
+
 class TestNode(unittest.TestCase):
+
 	def setUp(self):
 		self.test1 = Node('test1', 123)
 		self.test2 = Node('test2', 345, self.test1)
@@ -9,13 +14,15 @@ class TestNode(unittest.TestCase):
 		self.test1.setNext(self.test2)
 		self.test2.setNext(self.test1)
 
-	def test_return(self):
+
+	def testReturn(self):
 		self.assertEqual(self.test1.returnName(), 'test1')
 		self.assertEqual(self.test1.returnValue(), 123)
 		self.assertEqual(self.test1.returnPrev(), None)
 		self.assertEqual(self.test1.returnNext(), self.test2)
 
-	def test_set(self):
+
+	def testSet(self):
 		self.test1.setName('foo1')
 		self.assertEqual(self.test1.returnName(), 'foo1')
 		self.test1.setValue(321)
@@ -25,5 +32,22 @@ class TestNode(unittest.TestCase):
 		self.test1.setNext(self.test3)
 		self.assertEqual(self.test1.returnNext(), self.test3)
 
+
+	def testSetAll(self):
+		name = 'setAll'
+		value = 1337
+		prev = self.test2
+		n = self.test3
+		
+		self.test1.setName(name)
+		self.test1.setValue(value)
+		self.test1.setPrev(prev)
+		self.test1.setNext(n)
+
+		self.assertEqual(self.test1.returnName(), name)
+		self.assertEqual(self.test1.returnValue(), value)
+		self.assertEqual(self.test1.returnPrev(), prev)
+		self.assertEqual(self.test1.returnNext(), n)
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
