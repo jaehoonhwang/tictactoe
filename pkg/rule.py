6050054ea4867f1049c18ab892006cf5f327f99a
Jaehoon Hwang <jaehoon217@gmail.com>
2016-11-21 16:34:26 -0800

    Last Patch for the day: Add Attributes and Methods
    Added getter and setter and attribute, _Grid.

--- pkg/rule.py
+++ pkg/rule.py
@@ -9,3 +9,15 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.propagate = False
 logger.disabled = False
+
+
+class Rule(Grid):
+
+    def __init__(self, grid):
+        self._Grid = grid
+
+    def setGrid(self, grid):
+        self._Grid = grid
+
+    def returnGrid(self):
+        return self._Grid
